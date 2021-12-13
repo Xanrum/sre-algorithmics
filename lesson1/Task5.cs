ed4169283a082f3f5a164a16694b98967e87e1bc
Roman Nikolaev <r.nikolaev@tinkoff.ru>
2021-12-13 09:41:23 +0300

    Задача №5

--- lesson1/Task5.cs
+++ lesson1/Task5.cs
@@ -5,9 +5,37 @@ public class Lesson1Task5
 {
     [Theory]
     [InlineData(new[] { -1, 0, 1, 1, 1, 3, 5, 5, 6, 7 }, 1)]
+    [InlineData(new[] { 1, 1, 3, 3, 3, 5, 5, 5, 6 }, 3)] // 3 и 5 встречаются по три раза — берем первое
+    [InlineData(new[] { 10 }, 10)] // и такое бывает
     private void CheckSearchMostFValue(int[] list, int expected) => Assert.Equal(expected, SearchMostFValue(list));
 
-    // Дан отсортириованный массив. Найти наиболее встречающееся значение
-    // Если таких значений несколько - найти первое такое значение
-    private int SearchMostFValue(int[] list) => throw new NotImplementedException();
+    // Дан отсортириованный массив. Найти наиболее встречающееся значение.
+    // Если таких значений несколько — найти первое такое значение.
+    private int SearchMostFValue(int[] list) {
+        int count = 0;
+        int value = 0;
+        int fCount = 0;
+        int fValue = 0;
+
+        foreach (int item in list) {
+            if (count == 0) {
+                value = item;
+            }
+            else {
+                if (value != item) {
+                    if (fCount < count) {
+                        fCount = count;
+                        fValue = value;
+                    }
+
+                    value = item;
+                    count = 0;
+                }
+            }
+
+            count++;
+        }
+
+        return list.Length == 1 ? list[0] : fValue;
+    }
 }
