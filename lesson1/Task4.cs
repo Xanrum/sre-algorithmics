53dd5e6b8550c633515c2d46e48f2285e903caad
Roman Nikolaev <r.nikolaev@tinkoff.ru>
2021-12-20 23:22:25 +0300

    Оптимизировал + добавил тест

--- lesson1/Task4.cs
+++ lesson1/Task4.cs
@@ -4,6 +4,7 @@ using Xunit;
 public class Lesson1Task4
 {
     [Theory]
+    [InlineData(new[] { 0 }, new[] { 0 }, new[] { 0, 0 })]
     [InlineData(new[] { 0, 1, 1, 5, 6 }, new[] { -1, 1, 3, 5, 7 }, new[] { -1, 0, 1, 1, 1, 3, 5, 5, 6, 7 })]
     [InlineData(new[] { -10, -3, -2, 0, 1, 1, 5, 6 }, new[] { -1, 1, 3, 5, 7 }, new[] { -10, -3, -2, -1, 0, 1, 1, 1, 3, 5, 5, 6, 7 })] // вариант с листами разной длины
     private void CheckSearchMin(int[] listA, int[] listB, int[] expected) =>
@@ -15,31 +16,29 @@ public class Lesson1Task4
         int[] listC = new int[listA.Length + listB.Length];
         int indexA = 0;
         int indexB = 0;
-        int indexC = 0;
+        int indexC = -1;
 
         do {
+            indexC++;
+
             if (indexA >= listA.Length) {
                 listC[indexC] = listB[indexB];
-                indexC++;
                 indexB++;
                 continue;
             }
 
             if (indexB >= listB.Length) {
                 listC[indexC] = listA[indexA];
-                indexC++;
                 indexA++;
                 continue;
             }
 
             if (listA[indexA] <= listB[indexB]) {
                 listC[indexC] = listA[indexA];
-                indexC++;
                 indexA++;
             }
             else {
                 listC[indexC] = listB[indexB];
-                indexC++;
                 indexB++;
             }
         } while (indexA < listA.Length || indexB < listB.Length);
