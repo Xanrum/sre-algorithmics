d5cfb4a489cb532cd998d9e8cd581933c3e5d711
Roman Nikolaev <r.nikolaev@tinkoff.ru>
2021-12-21 00:00:34 +0300

    Переписал foreach на for + добавил тесты

--- lesson1/Task5.cs
+++ lesson1/Task5.cs
@@ -7,6 +7,9 @@ public class Lesson1Task5
     [InlineData(new[] { -1, 0, 1, 1, 1, 3, 5, 5, 6, 7 }, 1)]
     [InlineData(new[] { 1, 1, 3, 3, 3, 5, 5, 5, 6 }, 3)] // 3 и 5 встречаются по три раза — берем первое
     [InlineData(new[] { 10 }, 10)] // и такое бывает
+    [InlineData(new[] { 0, 1, 1 }, 1)]
+    [InlineData(new[] { 1, 1 }, 1)]
+    [InlineData(new[] { 0, 1 }, 0)]
     private void CheckSearchMostFValue(int[] list, int expected) => Assert.Equal(expected, SearchMostFValue(list));
 
     // Дан отсортированный массив. Найти наиболее встречающееся значение.
@@ -14,28 +17,25 @@ public class Lesson1Task5
     // O(n)
     private int SearchMostFValue(int[] list) {
         int count = 0;
-        int value = 0;
+        int value = list[0];
         int fCount = 0;
         int fValue = 0;
 
-        foreach (int item in list) {
-            if (count == 0) {
-                value = item;
-            }
+        if (list.Length <= 2) return list[0];
 
-            if (value != item) {
-                if (fCount < count) {
+        for (int i = 1; i < list.Length; i++) {
+            if (list[i] != value) {
+                if (count > fCount) {
                     fCount = count;
                     fValue = value;
                 }
-
-                value = item;
+                value = list[i];
                 count = 0;
             }
 
             count++;
         }
 
-        return list.Length == 1 ? list[0] : fValue;
+        return count > fValue ? value : fValue;
     }
 }
