b0671cddb6f2d67cd7a5761e55a768e56eb4815f
Roman Nikolaev <r.nikolaev@tinkoff.ru>
2021-12-22 02:06:08 +0300

    L3T2

--- lesson3/Task2.cs
+++ lesson3/Task2.cs
@@ -5,10 +5,23 @@ public class Lesson3Task2
 {
     [Theory]
     [InlineData("1+2", 3)]
+    [InlineData("0+0", 0)]
+    [InlineData("1+2+3", 6)]
     private void CheckSumator(string input, int expected) =>
         Assert.Equal(expected, Sumator(input));
 
-    // Дано корректное арифметическое выражение состоящее только из цифр и знаков плюс +
-    // Требуется произвести вычисление значения выражения
-    private int Sumator(string input) => throw new NotImplementedException();
+    // Дано корректное арифметическое выражение, состоящее только из цифр и знаков плюс +.
+    // Требуется произвести вычисление значения выражения.
+    private int Sumator(string input) {
+        int sum = 0;
+
+        foreach (char item in input) {
+            if (item != '+') {
+                int number = (int)Char.GetNumericValue(item);
+                sum = sum + number;
+            }
+        }
+
+        return sum;
+    }
 }
