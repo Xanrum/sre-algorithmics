729957b950b98551f947dc05e0e1ae8bdfc0e1a8
e.o.gavrikova <e.o.gavrikova@tinkoff.ru>
2021-12-21 14:46:34 +0300

    last tasks

--- lesson3/Task3.cs
+++ lesson3/Task3.cs
@@ -1,15 +1,38 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 public class Lesson3Task3
 {
+    // O(n) - по времени, O(n) - по памяти
     [Theory]
     [InlineData("AAAABBBC", "A4B3C")]
     [InlineData("AAAABBBCAAA", "A4B3CA3")]
+    [InlineData("ABBBCAAA", "AB3CA3")]
     private void CheckRLE(string input, string expected) =>
         Assert.Equal(expected, RLE(input));
 
     // Дана строка из латинских заглавных букв.
     // Необходимо заменить все повторы одинаковых подряд идущих букв на букву + цифру 
-    private string RLE(string input) => throw new NotImplementedException();
+    private string RLE(string input)
+    {
+        var result = new List<string>();
+        var count = 1;
+        var i = 1;
+        while (i < input.Length)
+        {
+            if (input[i] == input[i - 1])
+                count++;
+            else
+            {
+                result.Add($"{input[i - 1]}{(count > 1 ? count : "")}");
+                count = 1;
+            }
+
+            i++;
+        }
+
+        result.Add($"{input[i - 1]}{(count > 1 ? count : "")}");
+        return string.Join("",result);
+    }
 }
--- lesson3/Task4.cs
+++ lesson3/Task4.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 public class Lesson3Task4
 {
+    // O(n) - по времени, O(n) - по памяти (в хужшем случае?)
     [Theory]
-    [InlineData(new [] {1, 2, 3}, new [] {1, 2, 4})]
+    [InlineData(new[] {1, 2, 3}, new[] {1, 2, 4})]
+    [InlineData(new[] {1, 2, 9}, new[] {1, 3, 0})]
+    [InlineData(new[] {9, 9, 9}, new[] {1, 0, 0, 0})]
+    [InlineData(new[] {9, 8, 9}, new[] {9, 9, 0})]
     private void CheckInc(int[] input, int[] expected) =>
         Assert.Equal(expected, Inc(input));
 
@@ -12,5 +17,28 @@ public class Lesson3Task4
     // Массив представляет число в десятичной системе счисления
     // Функция производит инкремент +1
     // Длина массива до 1000
-    private int[] Inc(int[] input) => throw new NotImplementedException();
+    private int[] Inc(int[] input)
+    {
+        var f = 1;
+        for (var i = input.Length - 1; i >= 0; i--)
+        {
+            if (input[i] == 9)
+                input[i] = 0;
+
+            else
+            {
+                input[i]++;
+                f = 0;
+                break;
+            }
+        }
+
+        if (f == 1)
+        {
+            var result = new int [input.Length + 1];
+            result[0] = 1;
+            return result;
+        }
+        return input;
+    }
 }
