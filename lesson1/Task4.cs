9dc4e60851ca9bfdcfcbf19f02b0043b8f76062a
e.o.gavrikova <e.o.gavrikova@tinkoff.ru>
2021-12-22 12:50:10 +0300

    То же самое, только в профиль

--- lesson1/Task4.cs
+++ lesson1/Task4.cs
@@ -7,10 +7,11 @@ public class Lesson1Task4
 {
     // O(n) - по вермени, O(n) - по памяти. Считается же только доп память? 
     [Theory]
-    [InlineData(new[] { 0, 1, 1, 5, 6 }, new[] { -1, 1, 3, 5, 7 }, new[] { -1, 0, 1, 1, 1, 3, 5, 5, 6, 7 })]
-    [InlineData(new[] { 0 }, new[] { 1 }, new[] { 0, 1 })]
-    [InlineData(new[] { 0, 1, 1, 5, 6, 7, 8, 8, 9 }, new[] { -1, 1, 3, 5, 7 }, new[] { -1, 0, 1, 1, 1, 3, 5, 5, 6, 7, 7, 8, 8, 9 })]
-    [InlineData(new[] { 0, 1, 1, 5, 6 }, new[] { -1, 1, 3, 5, 7, 8, 9, 9 }, new[] { -1, 0, 1, 1, 1, 3, 5, 5, 6, 7, 8, 9, 9 })]
+    [InlineData(new[] {0, 1, 1, 5, 6}, new[] {-1, 1, 3, 5, 7}, new[] {-1, 0, 1, 1, 1, 3, 5, 5, 6, 7})]
+    [InlineData(new[] {0}, new[] {1}, new[] {0, 1})]
+    [InlineData(new[] {0, 1, 1, 5, 6, 7, 8, 8, 9}, new[] {-1, 1, 3, 5, 7},
+        new[] {-1, 0, 1, 1, 1, 3, 5, 5, 6, 7, 7, 8, 8, 9})]
+    [InlineData(new[] {0, 1, 1, 5, 6}, new[] {-1, 1, 3, 5, 7, 8, 9, 9}, new[] {-1, 0, 1, 1, 1, 3, 5, 5, 6, 7, 8, 9, 9})]
     private void CheckSearchMin(int[] listA, int[] listB, int[] expected) =>
         Assert.Equal(expected, Merge(listA, listB));
 
@@ -18,50 +19,90 @@ public class Lesson1Task4
     private int[] Merge(int[] listA, int[] listB)
     {
         var result = new int[listA.Length + listB.Length];
-        
+
         var i = 0;
         var j = 0;
         var k = 0;
-        while (true)
+        while (i < listA.Length && j < listB.Length)
         {
-            // хвосты
-            if (i == listA.Length)
-            {
-                while (j < listB.Length)
-                {
-                    result[k] = listB[j];
-                    k++;
-                    j++;
-                }
-                break;
-            }
-
-            if (j == listB.Length)
-            {
-                while (i < listA.Length)
-                {
-                    result[k]=listA[i];
-                    k++;
-                    i++;
-                } 
-                break;
-            }
-            
             if (listA[i] <= listB[j])
             {
-                result[k]=listA[i];
-                k++;
+                result[k] = listA[i];
                 i++;
-                continue;
             }
-            if (listA[i] > listB[j])
+            else
             {
-                result[k]=listB[j];
-                k++;
+                result[k] = listB[j];
                 j++;
             }
+
+            k++;
+        }
+
+        while (j < listB.Length)
+        {
+            result[k] = listB[j];
+            k++;
+            j++;
+        }
+
+        while (i < listA.Length)
+        {
+            result[k] = listA[i];
+            k++;
+            i++;
         }
 
         return result;
     }
+    
+    private int[] Merge_old(int[] listA, int[] listB)
+        {
+            var result = new int[listA.Length + listB.Length];
+            
+            var i = 0;
+            var j = 0;
+            var k = 0;
+            while (true)
+            {
+                // хвосты
+                if (i == listA.Length)
+                {
+                    while (j < listB.Length)
+                    {
+                        result[k] = listB[j];
+                        k++;
+                        j++;
+                    }
+                    break;
+                }
+    
+                if (j == listB.Length)
+                {
+                    while (i < listA.Length)
+                    {
+                        result[k]=listA[i];
+                        k++;
+                        i++;
+                    } 
+                    break;
+                }
+                
+                if (listA[i] <= listB[j])
+                {
+                    result[k]=listA[i];
+                    k++;
+                    i++;
+                    continue;
+                }
+                if (listA[i] > listB[j])
+                {
+                    result[k]=listB[j];
+                    k++;
+                    j++;
+                }
+            }
+    
+            return result;
+        }
 }
--- lesson2/Task3.cs
+++ lesson2/Task3.cs
@@ -4,21 +4,123 @@ using Xunit;
 
 public class Lesson2Task3
 {
-    // Вроде и O(n), а вроде и совсем не нравится, как решила 
+    // O(n) по времени, O(n) - по памяти.
+    // Еще одно тупое решение, не верю, что нельзя проще, но не придумала
     [Theory]
-    [InlineData(new[] { -1, 1, 1, 3, 5 }, new[] { 0, 1, 5, 6, 6, 6, 7 }, new[] { 1, 6 })]
+    [InlineData(new[] {-1, 1, 1, 3, 5}, new[] {0, 1, 5, 6, 6, 6, 7}, new[] {1, 6})]
     [InlineData(new[] {-1, 1, 1, 3, 5}, new[] {0, 1, 5, 6, 6, 6, 7, 7, 7, 7, 8}, new[] {7})]
-    [InlineData(new[] { 0 }, new[] { 0 }, new[] { 0 })]
-    [InlineData(new[] { 0 }, new[] { 1 }, new[] { 0, 1 })]
-    private void CheckSearchMostFValue2(int[] listA, int[] listB, int[] expected) =>
-        Assert.Equal(expected, SearchMostFValue2(listA, listB));
+    [InlineData(new[] {0}, new[] {0}, new[] {0})]
+    [InlineData(new[] {0}, new[] {1}, new[] {0, 1})]
+    [InlineData(new[] {0}, new[] {1, 2, 3}, new[] {0, 1, 2, 3})]
+    private void CheckSearchMostFValue(int[] listA, int[] listB, int[] expected) =>
+        Assert.Equal(expected, SearchMostFValue(listA, listB));
 
     // Даны два отсортириованный массива. Найти наиболее встречающееся значения в объединении двух массивов
     // Результат - отсортированный массив значений
+
+    private int[] SearchMostFValue(int[] listA, int[] listB)
+    {
+        var resultValues = new List<int>();
+        var i = 0;
+        var j = 0;
+        var resultFrequency = 1;
+        var counter = 1;
+        int current;
+        int previous;
+
+        if (listA[i] <= listB[j])
+        {
+            current = listA[i];
+            resultValues.Add(current);
+            i++;
+        }
+        else
+        {
+            current = listB[j];
+            resultValues.Add(current);
+            j++;
+        }
+        
+        while (i < listA.Length && j < listB.Length)
+        {
+            previous = current;
+            if (listA[i] <= listB[j])
+            {
+                current = listA[i];
+                i++;
+            }
+            else
+            {
+                current = listB[j];
+                j++;
+            }
+
+            if (current == previous)
+                counter++;
+            else
+            {
+                if (counter == resultFrequency) resultValues.Add(counter > 1 ? previous : current);
+                if (counter > resultFrequency)
+                {
+                    resultFrequency = counter;
+                    resultValues.Clear(); // Наверно тут можно использовать магию ArrayPool
+                    resultValues.Add(previous);
+                }
+                counter = 1;
+            }
+        }
+        
+        
+
+        while (j < listB.Length)
+        {
+            previous = current;
+            current = listB[j];
+            if (current == previous)
+                counter++;
+            else
+            {
+                if (counter == resultFrequency) resultValues.Add(counter > 1 ? previous : current);
+                if (counter > resultFrequency)
+                {
+                    resultFrequency = counter;
+                    resultValues.Clear();
+                    resultValues.Add(previous);
+                }
+                counter = 1;
+            }
+            j++;
+        }
+
+        while (i < listA.Length)
+        {
+            previous = current;
+            current = listA[i];
+            if (current == previous)
+                counter++;
+            else
+            {
+                if (counter == resultFrequency) resultValues.Add(counter > 1 ? previous : current);
+                if (counter > resultFrequency)
+                {
+                    resultFrequency = counter;
+                    resultValues.Clear();
+                    resultValues.Add(previous);
+                }
+                counter = 1;
+            }
+            i++;
+        }
+
+        return resultValues.ToArray();
+    }
+
+    
+    // старое неоптимальное решение
     private int[] SearchMostFValue2(int[] listA, int[] listB)
     {
         var resultDict = new Dictionary<int, int>();
-        var resultList = new List<int>(); // листы таки можно или нет? 
+        var resultList = new List<int>();  
         var list = Merge(listA, listB);
         Console.WriteLine(String.Join(", ", list));
 
@@ -29,7 +131,7 @@ public class Lesson2Task3
                 counter++;
             else
             {
-                resultDict.Add(list[i -1], counter);
+                resultDict.Add(list[i - 1], counter);
                 counter = 0;
             }
         }
@@ -44,7 +146,7 @@ public class Lesson2Task3
             if (element.Value == maxFrequency)
                 resultList.Add(element.Key);
         }
-        
+
         return resultList.ToArray();
     }
 
