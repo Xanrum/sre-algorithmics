d90d1e5867274e90280bfbd59a17acc18463d668
Roman Nikolaev <r.nikolaev@tinkoff.ru>
2021-12-27 00:00:43 +0300

    L4T1

--- lesson4/Task1.cs
+++ lesson4/Task1.cs
@@ -5,12 +5,21 @@ public class Lesson4Task1
 {
     [Theory]
     [InlineData(new [] {1, 2, 3, 4}, new[] {4, 3, 2, 1})]
+    [InlineData(new [] {0}, new[] {0})]  // массив из одного элемента
     private void CheckEvaluator(int[] input, int[] expected)
     {
         ReverseArray(input);
         Assert.Equal(expected, input);
     }
 
-    // требуется сделать реверс входного массива
-    private void ReverseArray(int[] input) => throw new NotImplementedException();
+    // Требуется сделать реверс входного массива.
+    private void ReverseArray(int[] input) {
+        int[] output = new int[input.Length];
+
+        for (int i = 0, o = input.Length-1; i < input.Length; i++, o--) {
+            output[o] = input[i];
+        }
+
+        output.CopyTo(input, 0);
+    }
 }
