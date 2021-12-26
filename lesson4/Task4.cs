479af0d30122362abad5680ee72abe8fdf13a2b9
Roman Nikolaev <r.nikolaev@tinkoff.ru>
2021-12-26 23:42:19 +0300

    L4T4

--- lesson4/Task4.cs
+++ lesson4/Task4.cs
@@ -5,6 +5,9 @@ public class Lesson4Task4
 {
     [Theory]
     [InlineData("2+2*2", 6)]
+    [InlineData("1+2", 3)]   // только сложение
+    [InlineData("3*4", 12)]  // только умножение
+    [InlineData("11*22*33+44*55", 10406)]  // всё вперемешку
     private void CheckEvaluator(string input, int expected) =>
         Assert.Equal(expected, Evaluator(input));
 
@@ -14,5 +17,21 @@ public class Lesson4Task4
     // цифр 0..9
     // строка представляет арифметическое выражение
     // требуется вычислить его значение, с учетом приоритета операций
-    private int Evaluator(string input) => throw new NotImplementedException();
+    private int Evaluator(string input) {
+        int multiplication = 1;
+        int result = 0;
+
+        string[] multiplications = input.Split('+');
+
+        foreach (string item in multiplications) {
+            foreach (string number in item.Split('*')) {
+                multiplication = multiplication * Int32.Parse(number);
+            }
+
+            result = result + multiplication;
+            multiplication = 1;
+        }
+
+        return result;
+    }
 }
