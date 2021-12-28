6462cbc66f2c4896508f21f84f635823f9e680a6
Roman Nikolaev <r.nikolaev@tinkoff.ru>
2021-12-29 00:19:17 +0300

    L4T3

--- lesson4/Task3.cs
+++ lesson4/Task3.cs
@@ -7,10 +7,58 @@ public class Lesson4Task3
     [InlineData("A4B3C", "AAAABBBC")]
     [InlineData("A4B3CA3", "AAAABBBCAAA")]
     [InlineData("(A2B)3Z", "AABAABAABZ")]
-    private void CheckEvaluator(string input, string expected) =>
-        Assert.Equal(expected, ReversRLE(input));
+    private void CheckReverseRLE(string input, string expected) =>
+        Assert.Equal(expected, ReverseRLE(input));
 
-    // распаковка RLE со скобками
-    // поdторяющиеся блоки могут быть объеденены в скобки
-    private string ReversRLE(string input) => throw new NotImplementedException();
+    // Распаковка RLE со скобками,
+    // повторяющиеся блоки могут быть объеденены в скобки.
+    private string ReverseRLE(string input) {
+
+        string ExtractRLE(string str) {
+            string rle = "";
+
+            for (int i = 0; i < str.Length; i++) {
+                int n = str[i] - '0';
+
+                if (n >= 1 && n <= 9) {
+                    while (n > 1) {
+                        rle = rle + str[i-1];
+                        n--;
+                    }
+                }
+                else {
+                    rle = rle + str[i];
+                }
+            }
+
+            return rle;
+        }
+
+
+        string output = "";
+
+        for (int i = 0; i < input.Length; i++) {
+            if (input[i] == '(') {
+                i++;
+                while (input[i] != ')') {
+                    output = output + input[i];
+                    i++;
+                }
+                i++;
+
+                int number = input[i] - '0';
+                string extractedRLE = ExtractRLE(output);
+
+                while (number > 1) {
+                    output = output + extractedRLE;
+                    number--;
+                }
+            }
+            else {
+                output = output + input[i];
+            }
+        }
+
+        return ExtractRLE(output);
+    }
 }
