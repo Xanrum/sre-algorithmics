50492d13c073cfd2c582c7725bec500216f9cadf
Rustam Sultanov <rustamelutingol@gmail.com>
2021-12-28 20:22:18 +0300

    4 task 1

--- lesson4/Task1.cs
+++ lesson4/Task1.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Linq;
 using Xunit;
 
 public class Lesson4Task1
 {
     [Theory]
-    [InlineData(new [] {1, 2, 3, 4}, new[] {4, 3, 2, 1})]
+    [InlineData(new[] { 0 }, new[] { 0 })]
+    [InlineData(new[] { 1, 2, 3, 4, 5 }, new[] { 5, 4, 3, 2, 1 })]
+    [InlineData(new[] { 1, 2, 3, 4 }, new[] { 4, 3, 2, 1 })]
     private void CheckEvaluator(int[] input, int[] expected)
     {
         ReverseArray(input);
@@ -12,5 +15,17 @@ public class Lesson4Task1
     }
 
     // требуется сделать реверс входного массива
-    private void ReverseArray(int[] input) => throw new NotImplementedException();
+    // O(N) память О(1)
+    private void ReverseArray(int[] input)
+    {
+        var (begin, end) = (0, input.Length - 1);
+        while (begin < end)
+        {
+            (input[begin], input[end]) = (input[end], input[begin]);
+            begin++;
+            end--;
+        }
+    }
+
+    private void ReverseArrayExtended(int[] input) => Array.Reverse(input);
 }
--- lesson4/Task4.cs
+++ lesson4/Task4.cs
@@ -4,6 +4,14 @@ using Xunit;
 public class Lesson4Task4
 {
     [Theory]
+    [InlineData("1+2", 3)]
+    [InlineData("123+257", 380)]
+    [InlineData("123+257+23", 403)]
+    [InlineData("+123+257+23", 403)]
+    [InlineData("257", 257)]
+    [InlineData("+257", 257)]
+    [InlineData("1+20", 21)]
+    [InlineData("1*20", 20)]
     [InlineData("2+2*2", 6)]
     private void CheckEvaluator(string input, int expected) =>
         Assert.Equal(expected, Evaluator(input));
@@ -15,4 +23,29 @@ public class Lesson4Task4
     // строка представляет арифметическое выражение
     // требуется вычислить его значение, с учетом приоритета операций
     private int Evaluator(string input) => throw new NotImplementedException();
+    // {
+    //     var plusPos = 0;
+    //     var mulPos = 0;
+    //     var sum = 0;
+    //     for (var i = 0; i < input.Length; i++)
+    //     {
+    //         if (i == 0 && input[i] == '+')
+    //         {
+    //             plusPos = 1;
+    //             continue;
+    //         }
+    //         if (i == 0 && input[i] == '*')
+    //         {
+    //             plusPos = 1;
+    //             continue;
+    //         }
+    //
+    //         if (input[i] != '+') continue;
+    //         sum += int.Parse(input[plusPos..i]);
+    //         plusPos = i;
+    //     }
+    //
+    //     sum += int.Parse(input[plusPos..]);
+    //     return sum;
+    // }
 }
