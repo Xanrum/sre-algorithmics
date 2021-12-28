93c5940879c0fc369e456ce444d2385552c11be2
e.o.gavrikova <e.o.gavrikova@tinkoff.ru>
2021-12-28 15:23:48 +0300

    какие-то решения

--- lesson4/Task1.cs
+++ lesson4/Task1.cs
@@ -3,8 +3,10 @@ using Xunit;
 
 public class Lesson4Task1
 {
+    // O(n) - по времени, O(1) - по памяти
     [Theory]
     [InlineData(new [] {1, 2, 3, 4}, new[] {4, 3, 2, 1})]
+    [InlineData(new [] {1, 2, 3, 4, 5}, new[] {5, 4, 3, 2, 1})]
     private void CheckEvaluator(int[] input, int[] expected)
     {
         ReverseArray(input);
@@ -12,5 +14,13 @@ public class Lesson4Task1
     }
 
     // требуется сделать реверс входного массива
-    private void ReverseArray(int[] input) => throw new NotImplementedException();
+    private void ReverseArray(int[] input)
+    {
+        for (var i = 0; i < input.Length / 2; i++)
+        {
+            var tmp = input[input.Length - 1 - i];
+            input[input.Length - 1 - i] = input[i];
+            input[i] = tmp;
+        }
+    }
 }
--- lesson4/Task3.cs
+++ lesson4/Task3.cs
@@ -1,16 +1,101 @@
 using System;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
 using Xunit;
 
 public class Lesson4Task3
 {
+    // Ну, это говнокод.
     [Theory]
     [InlineData("A4B3C", "AAAABBBC")]
+    [InlineData("A4BC2", "AAAABCC")]
     [InlineData("A4B3CA3", "AAAABBBCAAA")]
+    [InlineData("A4B10CA3", "AAAABBBBBBBBBBCAAA")]
     [InlineData("(A2B)3Z", "AABAABAABZ")]
+    [InlineData("(A2B)10Z", "AABAABAABAABAABAABAABAABAABAABZ")]
     private void CheckEvaluator(string input, string expected) =>
         Assert.Equal(expected, ReversRLE(input));
 
     // распаковка RLE со скобками
     // поdторяющиеся блоки могут быть объеденены в скобки
-    private string ReversRLE(string input) => throw new NotImplementedException();
+    private string ReversRLE(string input)
+    {
+        char letter = default;
+        List<char> result = new List<char>();
+        int i = 0;
+        while (i < input.Length)
+        {
+            if (char.IsLetter(input[i]))
+            {
+                if (letter != default)
+                {
+                    result.Add(letter);
+                }
+
+                letter = input[i];
+                i++;
+            }
+            else if (char.IsDigit(input[i]))
+            {
+                var start = i;
+                var count = 0;
+                while (i < input.Length && char.IsDigit(input[i]))
+                {
+                    count++;
+                    i++;
+                }
+                var number = int.Parse(input.AsSpan(start, count));
+                AddLetters(result, letter, number);
+                letter = default;
+            }
+            else if (input[i] == '(')
+            {
+                var count = 0;
+                i++;
+                var start = i;
+                while (input[i] != ')')
+                {
+                    count++;
+                    i++;
+                }
+
+                var res = ReversRLE(input.AsSpan(start, count).ToString());
+                var iterations = 1;
+                i++;
+                if (char.IsDigit(input[i]))
+                {
+                    start = i;
+                    count = 0;
+                    while (i < input.Length && char.IsDigit(input[i]))
+                    {
+                        count++;
+                        i++;
+                    }
+                }
+                iterations = int.Parse(input.AsSpan(start, count));
+                for (int j = 0; j < iterations; j++)
+                {
+                    foreach (var letterOfRes in res)
+                    {
+                        AddLetters(result, letterOfRes, 1);
+                    }
+                }
+
+                i++;
+            }
+        }
+
+        if (char.IsLetter(input[^1]))
+            result.Add(input[^1]);
+
+        return string.Join("", result);
+    }
+
+    private void AddLetters(List<char> result, char letter, int number)
+    {
+        for (int i = 0; i < number; i++)
+        {
+            result.Add(letter);
+        }
+    }
 }
