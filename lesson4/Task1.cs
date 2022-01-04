77eebafef338c7af648cde3ceba37291f27bd889
Roman Nikolaev <r.nikolaev@tinkoff.ru>
2022-01-04 22:53:58 +0300

    L4T1 — переписал без промежуточного массива

--- lesson4/Task1.cs
+++ lesson4/Task1.cs
@@ -14,12 +14,8 @@ public class Lesson4Task1
 
     // Требуется сделать реверс входного массива.
     private void ReverseArray(int[] input) {
-        int[] output = new int[input.Length];
-
-        for (int i = 0, o = input.Length-1; i < input.Length; i++, o--) {
-            output[o] = input[i];
+        for (int i = 0; i < input.Length/2; i++) {
+            (input[input.Length - 1 - i], input[i]) = (input[i], input[input.Length - 1 - i]);
         }
-
-        output.CopyTo(input, 0);
     }
 }
