175316dbae50e1af97dcecc3acf9bf0a61983815
e.o.gavrikova <e.o.gavrikova@tinkoff.ru>
2022-01-16 19:34:10 +0300

    5 - 2

--- lesson5/Task2.cs
+++ lesson5/Task2.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 public class Lesson5Task2
 {
     [Theory]
     [InlineData(new []{1, 2, 4}, 3)]
+    [InlineData(new []{4, 2, 1}, 3)]
+    [InlineData(new []{6, 7, 2, 4, 3, 1}, 5)]
     
     private void CheckFindSkip(int[] input, int expected) =>
         Assert.Equal(expected, FindSkip(input));
@@ -13,5 +16,15 @@ public class Lesson5Task2
     // в массиве находятся числа от 1 до N, но одно число исключили
     // надо найти исключенное число
     // на всякий случай - еще раз, массив НЕ отсортирован
-    private int FindSkip(int[] input) => throw new NotImplementedException();
+    private int FindSkip(int[] input)
+    {
+        var result = new List<int>();
+        for (int i = 1; i < input.Length + 1; i++)
+            result.Add(i);
+
+        foreach (var elem in input)
+            result.Remove(elem);
+
+        return result[0];
+    }
 }
